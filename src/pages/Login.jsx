0f5c43b3ce2140bc5124592b06cb4ec7c767ab85
mujtaba-ahmed12345 <dotnet.dev4@synergyintegratedsolutions.pk>
2026-02-10 23:52:10 +0500

    path issue on these file

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
+import { useAuth } from '../contexts/authContext';
 import { Mail, Lock, AlertCircle, Home } from 'lucide-react';
 
 export default function Login() {
